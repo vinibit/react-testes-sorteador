ae5062ad559e414e23e061ba746a8ed221c453f0
Vinicius Bitencourt Martins <vbmartins@prodam.sp.gov.br>
2025-04-30 17:19:17 -0300

    Atualiza dependencias (React Router e Recoil) e remove arquivos desnecessários

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
+import { BrowserRouter, Route, Routes } from 'react-router';
+import { RecoilRoot } from 'recoil';
+
 import Header from './components/Header/Header';
+import Form from './components/Form';
 
 function App() {
-  return (
-   <div className="App">
-      <Header />
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </div>
-  )
+    return (        
+        <>
+            <Header />
+            <BrowserRouter>
+                <RecoilRoot>
+                    <Routes>
+                        <Route path="/" element={<Form />} />
+                    </Routes>
+                </RecoilRoot>                           
+            </BrowserRouter>
+        </>
+    )
 }
 
-export default App;
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,13 @@
-import './index.css';
+import './index.css'
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+ReactDOM
+	.createRoot(document.getElementById('root') as HTMLElement)
+	.render(
+		<React.StrictMode>			
+			<App />			
+		</React.StrictMode>	
 );
-
-reportWebVitals();
